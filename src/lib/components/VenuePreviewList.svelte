8afe43bb6748d3ff1c9e46320d6f019dac8a348c
Leon van Wissen <l.vanwissen@uva.nl>
2025-03-18 13:39:54 +0100

    fix: remove debug

--- src/lib/components/VenuePreviewList.svelte
+++ src/lib/components/VenuePreviewList.svelte
@@ -4,8 +4,6 @@
 </script>
 
 <ul class="bg-base-100 grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-1">
-	<code>{JSON.stringify($filteredVisibleFeatures, null, 2)}</code>
-
 	{#each $filteredVisibleFeatures as feature, index (index)}
 		<VenuePreview venue={JSON.parse(feature.properties.venue)} id={feature.id ?? ''} />
 	{/each}
