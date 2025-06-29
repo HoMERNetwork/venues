34322903097144f7042318acc8a347aec28078fc
Leon van Wissen <l.vanwissen@uva.nl>
2025-06-29 15:28:02 +0200

    add warning and update deps

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -9,13 +9,31 @@
 	import Venue from '$lib/components/Venue.svelte';
 	import VenuePreviewList from '$lib/components/VenuePreviewList.svelte';
 
-	import { X, Search } from '@lucide/svelte';
+	import { X, Search, Info } from '@lucide/svelte';
 </script>
 
 {#if $selectedFeature}
-	<div class="h-full overflow-auto">
-		<div class="card-body flex h-full flex-col p-4 pb-0">
+	<div class="flex h-full flex-col overflow-auto">
+		<div class="card-body flex flex-grow flex-col p-4 pb-0">
 			<Venue bind:venue={$selectedFeature} />
+		</div>
+		<div class="mt-auto">
+			<div
+				class="alert bg-warning bg-opacity-20 border-warning text-warning-content m-4 mt-0 flex items-start gap-2 rounded-lg border-2 p-3 text-xs sm:text-sm"
+			>
+				<Info size={18} class="mt-0.5 flex-shrink-0" />
+				<div>
+					<strong class=" mb-1 block">Info</strong>
+					<p>
+						The venue locations on this map are derived from comprehensive researcher datasets.
+						While efforts are made to accurately present this information, minor inconsistencies may
+						occasionally arise during the data conversion process.
+						<br /><br />
+						For the most precise and authoritative information, consult the original source data or citations
+						that are provided.
+					</p>
+				</div>
+			</div>
 		</div>
 	</div>
 {:else}
